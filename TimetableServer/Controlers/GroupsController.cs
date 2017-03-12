2b7e69d3758da4762d46fdc6a9a77af0ef86bc6e
KamilRutkowski <kamilrutkowski1994@gmail.com>
2017-03-12 18:41:32 +0100

    Starting server side app. To see the result run it and paste after the socket number: /api/groups You will get result [{"name":"Informatyka","id":"XD","groups":[{"name":"Semestr 1","id":"XDNO","groups":null},{"name":"Semestr 2","id":"XDBEKA","groups":null}]}]

---
+++ TimetableServer/Controlers/GroupsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TimetableServer.Models;
+
+namespace TimetableServer.Controlers
+{
+    public class GroupsController : ApiController
+    {
+        public IEnumerable<Group> GetAllGroups()
+        {
+            return new List<Group>()
+            {
+                new Group() { id = "XD", name = "Informatyka", groups = new List<Group>()
+                {
+                    new Group() { id = "XDNO", name = "Semestr 1", groups = null},
+                    new Group() { id = "XDBEKA", name = "Semestr 2", groups = null}
+                } }
+            };
+        } 
+    }
+}
---
+++ TimetableServer/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimetableServer.Models
+{
+    public class Group
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public List<Group> groups { get; set; } 
+    }
+}
