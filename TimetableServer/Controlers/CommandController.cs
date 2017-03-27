30390814d2784b140393f9069c412b9eabfd92f8
KamilRutkowski <kamilrutkowski1994@gmail.com>
2017-03-27 12:45:03 +0200

    Post endpoint in progress

---
+++ TimetableServer/Controlers/CommandController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TimetableServer.Models;
+
+namespace TimetableServer.Controlers
+{
+    public class CommandController : ApiController
+    {
+        // POST: Command
+        public JObject Post([FromBody] Command value)
+        {
+            switch (value.type)
+            {
+                case "add_study_plan":
+                    return null;
+                case "add_classroom":
+                {
+                    var obj = JsonConvert.DeserializeObject<Classroom>(value.data.ToString());
+                    obj.id = "50";
+                    return JObject.Parse(JsonConvert.SerializeObject(obj));
+                }
+                case "add_teacher":
+                    return null;
+                case "select_start":
+                    return null;
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ TimetableServer/Models/Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace TimetableServer.Models
+{
+    public class Command
+    {
+        public string type { get; set; }
+        public JObject data { get; set; }
+    }
+}
