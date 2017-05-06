d2db29776e3f45643e417c425b6fde339ede2c0a
KamilRutkowski <kamilrutkowski1994@gmail.com>
2017-05-06 13:05:43 +0200

    Teachers and groups end poits work with DB

--- TimetableServer/Controlers/GroupsController.cs
+++ TimetableServer/Controlers/GroupsController.cs
@@ -10,19 +10,38 @@ namespace TimetableServer.Controlers
 {
     public class GroupsController : ApiController
     {
+        private DataBase db = new DataBase();
+
         public IEnumerable<Group> GetAllGroups()
         {
-            return new List<Group>()
+            db = db ?? new DataBase();
+            return
+                populateSubGroups(convertDBGroupsToGroups(db.getGroupsWithParentGroupId(null)));
+        }
+
+        private List<Group> convertDBGroupsToGroups(List<@group> dbGroups)
+        {
+            List<Group> Groups = new List<Group>();
+            foreach (var dbGroup in dbGroups)
             {
-                new Group()
-                {
-                    id = "XD", name = "Informatyka", groups = new List<Group>()
-                    {
-                        new Group() { id = "XDNO", name = "Semestr 1", groups = null},
-                        new Group() { id = "XDBEKA", name = "Semestr 2", groups = null}
-                    }
-                }
-            };
+                var gr = new Group();
+                gr.name = dbGroup.name;
+                gr.id = dbGroup.idgroups;
+                gr.groups = new List<Group>();
+                Groups.Add(gr);
+            }
+            return Groups;
+        }
+
+        private List<Group> populateSubGroups(List<Group> baseGroup)
+        {
+            if (baseGroup.Count == 0)
+                return baseGroup;
+            foreach (var group in baseGroup)
+            {
+                group.groups = populateSubGroups(convertDBGroupsToGroups(db.getGroupsWithParentGroupId(group.id)));
+            }
+            return baseGroup;
         }
     }
 }
