1c72715ad43084ac3782b9a2976bf9b62f988e4e
mwolszleger <mwolszleger@interia.pl>
2017-05-08 12:55:23 +0200

    Fixed JSON formatting

--- TimetableServer/App_Start/WebApiConfig.cs
+++ TimetableServer/App_Start/WebApiConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -32,7 +32,7 @@ namespace TimetableServer
             );
 
             //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
-            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+            config.Formatters.Add(new BrowserJsonFormatter());
         }
     }
 }
